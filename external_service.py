d856f3ce1db603485d2e652b58bab9b0fc5be36e
mubeen8870 <mubeen.shaik@thezennialpro.com>
2025-08-07 23:25:38 +0530

    Add files via upload

---
+++ external_service.py
@@ -0,0 +1,16 @@
+def fetch_live_news():
+    return{
+        "status": "ok",
+        "articles": [
+            {
+                "title": "Breaking: AI Revolution",
+                "content": "AI is transforming industires worldwide.",
+                "author": "OpenAI News"
+            },
+            {
+                "title": "Global Market Trends",
+                "content": "Markets are showing positive growth.",
+                "author": "Finance Daily"
+            }
+        ]
+    }
